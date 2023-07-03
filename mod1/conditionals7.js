fe60e09954ca7b66d6cf0e636e053541ddf3db7a
Thiem <thiem.huynh0@gmail.com>
2023-07-03 16:43:49 -0400

    min and max words in array sort update

--- mod1/conditionals7.js
+++ mod1/conditionals7.js
@@ -7,4 +7,42 @@ function areValidCredentials(name, password) {
         return false;
     }
     /* END SOLUTION */
+}
+
+
+// ******************************************************************************************************************************
+
+function findMinLengthOfThreeWords(word1, word2, word3) {
+    // your code here
+    /* START SOLUTION */
+    let arr = [word1, word2, word3];
+
+    arr = arr.sort(function(a,b){
+        // return b.length - a.length; //sorts by longest str length
+        return a.length - b.length;
+    });
+
+    return arr[0].length;
+
+    /* END SOLUTION */
+  }
+
+//   var output = findMinLengthOfThreeWords('500', 'be32423u', 'see7');
+// console.log(output); // --> 1
+
+
+// ******************************************************************************************************************************
+
+function findMaxLengthOfThreeWords(word1, word2, word3) {
+    // your code here
+    /* START SOLUTION */
+    let arr = [word1, word2, word3];
+
+    arr = arr.sort(function(a,b){
+        return b.length - a.length; //sorts by longest str length
+        // return a.length - b.length; //sorts by shortest str length
+    });
+
+    return arr[0].length;
+    /* END SOLUTION */
   }
