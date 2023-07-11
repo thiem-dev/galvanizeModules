88883f0901cc110ccc10aa98f04f5c0a30abdc4a
Thiem <thiem.huynh0@gmail.com>
2023-07-11 16:17:03 -0400

    updates

--- mod1-part5/5arrayMethods14.js
+++ mod1-part5/5arrayMethods14.js
@@ -1,3 +1,25 @@
 function joinArrayOfArrays(arr) {
     // your code here
+
+    let finalArr = [];
+
+    // console.log(finalArr.concat(arr[0]));
+    // console.log(finalArr.concat(arr[1]));
+
+    for(let i = 0; i < arr.length; i++){
+        if(!Array.isArray(arr[i]) || arr[i] === undefined){
+            continue;
+        } else if(arr[i].length === 0){
+            continue;
+        }
+
+        finalArr = finalArr.concat(arr[i]);
+    }
+
+    // console.log(finalArr);
+
+    return finalArr;
   }
+
+  var output = joinArrayOfArrays([[1, 4], [true, false], ['x', 'y']]);
+console.log(output); // --> [1, 4, true, false, 'x', 'y']
