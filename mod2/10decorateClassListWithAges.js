ec7317853290b60b13a715374ee4d909ba54f333
Thiem <thiem.huynh0@gmail.com>
2023-07-21 13:25:12 -0400

    update

--- mod2/10decorateClassListWithAges.js
+++ mod2/10decorateClassListWithAges.js
@@ -16,23 +16,51 @@ function decorateClassListWithAges(classList) {
         classNamesWithAges[name] = getRandomIntInclusive(10, 11);
     }
 
+
+    
+    // returns an array of these objects
+
     // console.log(classNamesWithAges);
     return classNamesWithAges;
 
 
-    // returns an array of these objects
 }
   
-    // ASSERTION FUNCTION(S) TO BE USED
+    // ASSERTION FUNCTION(S) TO BE USED //expected not used
+function assertClassListAges(classList, testName){ 
+
+    let objActual = decorateClassListWithAges(classList);
+    let condition = false;
+
+    for(const [key, value] of Object.entries(objActual)){
+        if(value === 11 || value === 10){
+            condition = true;
+        }
+    }
+
+    if(condition){
+        // console.log("passed");
+        return `passed [ ${testName} ]`
+    } else{
+        // console.log(`FAILED [ ${testName} ]`);
+        return `FAILED [ ${testName} ]`
+    }
+}
 
     // TESTS CASES
 
 
 
 
-var classList = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
+var classList1 = ["Joe", "Jack", "John", "Fred", "Frank", "Barry", "Larry", "Mary",
 "Harry", "Farrell", "Susan", "Monica", "Keira", "Caroline", "Harriet", "Erica",
 "Luann", "Cheryl", "Beth", "Rupa", "Linda", "Allison", "Nancy", "Dora"];
 
-let output = decorateClassListWithAges(classList)
+let output = decorateClassListWithAges(classList1)
 console.log(output)
+
+let output2 = assertClassListAges(classList1, 'assertClassListAgesPassing')
+console.log(output2);
+
+// let output3 = assertClassListAges(testActual, 'assertClassListAgesActual');
+// console.log(output3);
